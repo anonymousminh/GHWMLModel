f8fe041b26a32ae28b7ad05fc7c729c294237ba9
Minh <anhminh7802@gmail.com>
2025-12-13 16:55:12 -0700

    feat: implement functions to load standard and biased student performance data

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def load_standard_data(path):
+    df = pd.read_csv('../data/Student_Performance_Dataset.csv')
+    allowed_columns = [
+        'Attendance (%)',
+        'Midterm_Score',
+        'Final_Score',
+        'Projects_Score',
+        'Study_Hours_per_Week'
+    ]
+    return df[allowed_columns]
+
+def load_biased_data(path):
+    df = pd.read_csv('../data/Student_Performance_Dataset.csv')
+    biased_allowed_columns = [
+        'Attendance (%)',
+        'Midterm_Score',
+        'Final_Score',
+        'Project_Score',
+        'Study_Hours_per_Week',
+        'Gender',
+        'Parent_Education',
+        'Internet_Access_at_Home'
+    ]
